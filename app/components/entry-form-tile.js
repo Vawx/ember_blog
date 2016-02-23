8f2c11074206f7108d0b33b1dff9a3e5d3c2b1bb
kyle <altck8@gmail.com>
2016-02-22 16:06:47 -0800

    can add new entries with titles

--- app/components/entry-form-tile.js
+++ app/components/entry-form-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveEntry( ) {
+      var params = {
+        content: this.get('content'),
+        title: this.get('title'),
+      };
+      this.sendAction('saveEntry', params);
+    },
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,10 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend( {
   model() {
-    console.log(this.store.findAll("entry"));
     return Ember.RSVP.hash({
       posts: this.store.findAll("post"),
       entries: this.store.findAll("entry"),
     });
-  }
+  },
 });
